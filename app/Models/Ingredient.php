2d19cb037c853db8c9713c45b330950124abb202
khaled abodaif <khaled.abodaif@yahoo.com>
2025-01-21 02:25:21 +0200

    create Ingredient model

---
+++ app/Models/Ingredient.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\UnitEnum;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Ingredient extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'description',
+        'stock_quantity',
+        'stock_alert_sent',
+        'initial_stock_quantity',
+        'is_active',
+        'unit'
+    ];
+
+    protected $casts = [
+        'unit' => UnitEnum::class,
+        'stock_quantity' => 'decimal:3',
+        'stock_alert_sent' => 'boolean',
+    ];
+
+}
